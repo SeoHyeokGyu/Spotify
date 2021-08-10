2c661fc698a52a1001756fe842a3623b02e5deba
SeoHyeokGyu <shk64099@gmail.com>
2021-08-10 17:26:38 +0900

    #Add 1. API 콜 셋업

---
+++ Spotify/Models/RecommendationsResponse.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify
+//
+//  Created by 서혁규 on 2021/08/10.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
+
