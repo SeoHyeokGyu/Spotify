8980f67e833bbcd1e3f79fc4a1102960c524188e
SeoHyeokGyu <shk64099@gmail.com>
2021-08-24 17:29:44 +0900

    #Add
    1. LibraryToggleView, LibraryToggleViewDelegate 생성
    2. LibraryAlbumViewController, LibraryPlaylistViewController 생성
    3. LibraryViewController 에서 스크롤 뷰 와 토글 뷰 추가하여 앨범과 플레이리스트 볼 수 있게함

---
+++ Spotify/Controller/LibraryAlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryAlbumViewController.swift
+//  Spotify
+//
+//  Created by 서혁규 on 2021/08/24.
+//
+
+import UIKit
+
+class LibraryAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Spotify/Controller/LibraryPlaylistViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistViewController.swift
+//  Spotify
+//
+//  Created by 서혁규 on 2021/08/24.
+//
+
+import UIKit
+
+class LibraryPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+    }
+
+}
